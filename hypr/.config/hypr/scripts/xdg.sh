da06069013173c12d9bc7e820cd3df8a60ee764b
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-08 21:36:08 +0200

    Hyprland: Update xdg autostarts

--- hypr/.config/hypr/scripts/xdg.sh
+++ hypr/.config/hypr/scripts/xdg.sh
@@ -7,8 +7,18 @@
 #
 
 sleep 1
+
+# Kill all possible running xdg-desktop-portals
 killall -e xdg-desktop-portal-hyprland
-killall xdg-desktop-portal
+killall -e xdg-desktop-portal
+
+# Set required environment variables
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
+
+# Start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
+
+# Start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
+
