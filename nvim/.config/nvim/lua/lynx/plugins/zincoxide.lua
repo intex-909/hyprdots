dae6881c14a3301f1947e985a52c2704fe498721
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-08 18:47:49 +0200

    Neovim: Add new plugins cinnamon - smooth scroll highlight colors zoxide integration

---
+++ nvim/.config/nvim/lua/lynx/plugins/zincoxide.lua
@@ -0,0 +1,18 @@
+return {
+	-- You can also use the codeberg mirror if you want to use the plugin without relying on GitHub
+	-- "https://codeberg.org/CodingThunder/zincoxide.git" -- for HTTPS
+	-- "git@codeberg.org:CodingThunder/zincoxide.git"     -- for SSH
+	-- NOTE: the username on both github and codeberg are different
+	"thunder-coding/zincoxide",
+	opts = {
+		-- name of zoxide binary in your "$PATH" or path to the binary
+		-- the command is executed using vim.fn.system()
+		-- eg. "zoxide" or "/usr/bin/zoxide"
+		zincoxide_cmd = "zoxide",
+		-- Kinda experimental as of now
+		complete = true,
+		-- Available options { "tabs", "window", "global" }
+		behaviour = "tabs",
+	},
+	cmd = { "Z", "Zg", "Zt", "Zw" },
+}
