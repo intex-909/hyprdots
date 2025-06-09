53d78edc20a1f0d176164896d1e7d39effa5678b
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-09 15:21:08 +0200

    Hyprland: Update xdg scripts

--- hypr/.config/hypr/scripts/xdg.sh
+++ hypr/.config/hypr/scripts/xdg.sh
@@ -10,10 +10,16 @@ sleep 1
 
 # Kill all possible running xdg-desktop-portals
 killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
 killall -e xdg-desktop-portal
 
 # Set required environment variables
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
+sleep 1
 
 # Start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
@@ -21,4 +27,4 @@ sleep 2
 
 # Start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
-
+sleep 0.3
