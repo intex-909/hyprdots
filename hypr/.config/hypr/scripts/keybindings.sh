4a73e2f0594a04820eae4ce925ef96cfdabc821f
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-15 17:22:53 +0200

    Hyprland: Show keybindings in rofi

--- hypr/.config/hypr/scripts/keybindings.sh
+++ hypr/.config/hypr/scripts/keybindings.sh
@@ -1,39 +1,21 @@
+
 #!/bin/bash
-#  _              _     _           _ _
-# | | _____ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___
-# | |/ / _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|
-# |   <  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \
-# |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
-#           |___/                             |___/
-#
-# -----------------------------------------------------
-# Get keybindings location based on variation
-# -----------------------------------------------------
-config_file=$(<~/.config/hypr/conf/keybinding.conf)
-# config_file=${config_file//source = ~//home/$USER}
 
-# -----------------------------------------------------
-# Path to keybindings config file
-# -----------------------------------------------------
-echo "Reading from: $config_file"
+HYPR_CONF="$HOME/.config/hypr/conf/keybinding.conf"
 
-keybinds=$(awk -F'[=#]' '
-    $1 ~ /^bind/ {
-        # Replace the string "$mainMod" with "SUPER" (for the super key)
-        gsub(/\$mainMod/, "SUPER", $0)
+# extract the keybinding from hyprland.conf
+mapfile -t BINDINGS < <(grep '^bind=' "$HYPR_CONF" | \
+    sed -e 's/  */ /g' -e 's/bind=//g' -e 's/, /,/g' -e 's/ # /,/' -e 's/$mainMod/SUPER/g' | \
+    awk -F, -v q="'" '{cmd=""; for(i=3;i<NF;i++) cmd=cmd $(i) " ";print $1 " + " $2 " -- " $NF ",<span color=" q "gray" q "> " cmd "</span>"}')
 
-        # Remove "bind" and extra spaces, if any, at the beginning of the line
-        gsub(/^bind[[:space:]]*=+[[:space:]]*/, "", $0)
+CHOICE=$(printf '%s\n' "${BINDINGS[@]}" | rofi -dmenu -i -markup-rows -p "Hyprland Keybinds")
 
-        # Split the keybinding part (e.g., "Mod1,Return") using a comma
-        split($1, kbarr, ",")
+# extract cmd from span <span color='gray'>cmd</span>
+CMD=$(echo "$CHOICE" | sed -n 's/.*<span color='\''gray'\''>\(.*\)<\/span>.*/\1/p')
 
-        # Format the keybinding and associated command and prepare for output:
-        # Concatenate the two keybinding keys (e.g., "Mod1" + "Return") and append the command
-        print kbarr[1] "  + " kbarr[2] "\r" $2
-    }
-' "$config_file")
-
-sleep 0.2
-# rofi -dmenu -i -markup -eh 2 -replace -p "Keybinds" -config ~/.config/rofi/config-compact.rasi <<<"$keybinds"
-rofi -dmenu -i -markup -eh 2 -replace -p "Keybinds" <<<"$keybinds"
+# execute it if first word is exec else use hyprctl dispatch
+if [[ $CMD == exec* ]]; then
+    eval "$CMD"
+else
+    hyprctl dispatch "$CMD"
+fi
