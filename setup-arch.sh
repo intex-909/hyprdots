d175a42d0478ef9f7b06fc41362e3988abada1e7
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-24 19:33:52 +0200

    Add setup-arch script: start

---
+++ setup-arch.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+clear
+
+# -----------------------------------------------------
+# Download Folder
+# -----------------------------------------------------
+download_folder="/tmp/dotfiles_install"
+
+# Create download_folder if not exists
+if [ ! -d $download_folder ]; then
+    mkdir -p $download_folder
+fi
+
+# Check if package is installed
+_isInstalled() {
+    package="$1"
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+    if [ -n "${check}" ]; then
+        echo 0 #'0' means 'true' in Bash
+        return #true
+    fi
+    echo 1 #'1' means 'false' in Bash
+    return #false
+}
+
+# Check if command exists
+_checkCommandExists() {
+    package="$1"
+    if ! command -v $package >/dev/null; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Install required packages
+_installPackages() {
+    toInstall=()
+    for pkg; do
+        if [[ $(_isInstalled "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed."
+            continue
+        fi
+        toInstall+=("${pkg}")
+    done
+    if [[ "${toInstall[@]}" == "" ]]; then
+        # echo "All pacman packages are already installed.";
+        return
+    fi
+    printf "Package not installed:\n%s\n" "${toInstall[@]}"
+    sudo pacman --noconfirm -S "${toInstall[@]}"
+}
+
+# install yay if needed
+_installYay() {
+    _installPackages "base-devel"
+    SCRIPT=$(realpath "$0")
+    temp_path=$(dirname "$SCRIPT")
+    git clone https://aur.archlinux.org/yay.git $download_folder/yay
+    cd $download_folder/yay
+    makepkg -si
+    cd $temp_path
+    echo ":: yay has been installed successfully."
+}
+
+# Required packages for the installer
+packages=(
+    "wget"
+    "unzip"
+    "gum"
+    "rsync"
+    "git"
+)
+
+# Some colors
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+# Header
+echo -e "${GREEN}"
+cat <<"EOF"
+   ____         __       ____
+  /  _/__  ___ / /____ _/ / /__ ____
+ _/ // _ \(_-</ __/ _ `/ / / -_) __/
+/___/_//_/___/\__/\_,_/_/_/\__/_/
+
+EOF
+echo "Dotfiles for Hyprland"
+echo -e "${NONE}"
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (y/n): " yn
+    case $yn in
+        [Yy]*)
+            echo ":: Installation started."
+            echo
+            break
+            ;;
+        [Nn]*)
+            echo ":: Installation canceled"
+            exit
+            break
+            ;;
+        *)
+            echo ":: Please answer yes or no."
+            ;;
+    esac
+done
+
+# Create Download folder if not exists
+if [ ! -d $download_folder ]; then
+    mkdir -p $download_folder
+    echo ":: $download_folder folder created"
+fi
+
+# Remove existing download folder and zip files
+if [ -f $download_folder/dotfiles-main.zip ]; then
+    rm $download_folder/dotfiles-main.zip
+fi
+if [ -f $download_folder/dotfiles-dev.zip ]; then
+    rm $download_folder/dotfiles-dev.zip
+fi
+if [ -f $download_folder/dotfiles.zip ]; then
+    rm $download_folder/dotfiles.zip
+fi
+if [ -d $download_folder/dotfiles ]; then
+    rm -rf $download_folder/dotfiles
+fi
+if [ -d $download_folder/dotfiles_temp ]; then
+    rm -rf $download_folder/dotfiles_temp
+fi
+if [ -d $download_folder/dotfiles-main ]; then
+    rm -rf $download_folder/dotfiles-main
+fi
+if [ -d $download_folder/dotfiles-dev ]; then
+    rm -rf $download_folder/dotfiles-dev
+fi
+
+# Synchronizing package databases
+sudo pacman -Sy
+echo
+
+# Install required packages
+echo ":: Checking that required packages are installed..."
+_installPackages "${packages[@]}"
+
+# Install yay if needed
+if _checkCommandExists "yay"; then
+    echo ":: yay is already installed"
+else
+    echo ":: The installer requires yay. yay will be installed now"
+    _installYay
+fi
+echo
+
+# Start Spinner
+gum spin --spinner dot --title "Starting setup now..." -- sleep 3
+
+# Start setup
+ml4w-hyprland-setup -p arch
