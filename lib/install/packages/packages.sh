8b3d588bdc6f018848bc2c77e3ada3b19f18a42d
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-28 17:36:21 +0200

    Setup: Update packages log styles

--- lib/install/packages/packages.sh
+++ lib/install/packages/packages.sh
@@ -5,32 +5,32 @@ _writeLogHeader "Packages"
 _writeHeader "Packages"
 
 # Base
-_writeHeader "Installing base packages"
+_writeHeader "Base"
 source $packages_directory/$install_platform/base.sh
 _installPackages "${packages[@]}"
 echo
 
 # Terminal tools
-_writeHeader "Installing terminal tools"
+_writeHeader "Terminal tools"
 source $packages_directory/$install_platform/terminal-tools.sh
 _installPackages "${packages[@]}"
 echo
 
 # Additional
-_writeHeader "Installing additional packages"
+_writeHeader "Additional"
 source $packages_directory/$install_platform/addons.sh
 _installPackages "${packages[@]}"
 echo
 
 # Install AUR packages
-_writeHeader "Installing AUR packages"
+_writeHeader "AUR packages"
 source $packages_directory/$install_platform/aur-packs.sh
 _installAurPackages "${packages[@]}"
 echo
 
 # Install Hyprland
 if gum confirm "Do you want to install Hyprland packages?"; then
-    _writeHeader "Installing Hyprland packages"
+    _writeHeader "Hyprland"
     
     source $packages_directory/$install_platform/hyprland.sh
     _installPackages "${packages[@]}"
@@ -39,7 +39,7 @@ echo
 
 # Install fnm (Fast Node Manager)
 if gum confirm "Do you want to install fnm (Fast Node Manager)?"; then
-    _writeHeader "Installing Fast Node Manager"
+    _writeHeader "Fast Node Manager"
 
     curl -fsSL https://fnm.vercel.app/install | bash
 fi
@@ -49,7 +49,7 @@ echo
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
 if gum confirm "Do you want to install flatpaks?"; then
-    _writeHeader "Installing flatpaks"
+    _writeHeader "Flatpaks"
 
     # Install flatpaks
     flatpak install --user flathub com.github.tchx84.Flatseal
