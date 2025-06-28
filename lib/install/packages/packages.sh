158cb5f580c2c4f160d6e60bd345858cf0b1b872
intex-909 <68182191+intex-909@users.noreply.github.com>
2025-06-28 16:03:36 +0200

    Setup: Add optional flatpak installation

--- lib/install/packages/packages.sh
+++ lib/install/packages/packages.sh
@@ -24,11 +24,29 @@ _writeLogTerminal 0 "Installing AUR packages..."
 source $packages_directory/$install_platform/aur-packs.sh
 _installAurPackages "${packages[@]}"
 
+# Install Hyprland
 if gum confirm "Do you want to install Hyprland packages?"; then
     _writeLogTerminal 0 "Installing Hyprland packages..."
     
-    # Hyprland
     source $packages_directory/$install_platform/hyprland.sh
     _installPackages "${packages[@]}"
 fi
 
+# Install fnm (Fast Node Manager)
+if gum confirm "Do you want to install fnm (Fast Node Manager)?"; then
+    _writeLogTerminal 0 "Installing Fast Node Manager..."
+
+    curl -fsSL https://fnm.vercel.app/install | bash
+fi
+
+# Add Flathub repository
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+if gum confirm "Do you want to install flatpaks?"; then
+    _writeLogTerminal 0 "Installing flatpaks..."
+
+    # Install flatpaks
+    flatpak install --user flathub com.github.tchx84.Flatseal
+    flatpak install --user flathub io.github.flattool.Warehouse
+    flatpak install --user flathub org.gnome.World.PikaBackup
+    flatpak install --user flathub com.spotify.Client
+fi
